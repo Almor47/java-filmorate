13093c048a3d819ee35dde461905b898197e3ddf
Aleksandr <alex-moroz99@yandex.ru>
2023-04-06 19:02:01 +0300

    Исправление пустых строк и импортов

--- src/main/java/ru/yandex/practicum/filmorate/annotation/ReleaseDateConstraintValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/annotation/ReleaseDateConstraintValidator.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.annotation;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
+
 public class ReleaseDateConstraintValidator implements ConstraintValidator<ReleaseDateConstraint, LocalDate> {
     @Override
     public boolean isValid(LocalDate date, ConstraintValidatorContext constraintValidatorContext) {
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Builder;
 import lombok.Data;
 import ru.yandex.practicum.filmorate.annotation.ReleaseDateConstraint;
-
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
